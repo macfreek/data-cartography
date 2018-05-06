d7301bfd8fc0f16e3f118b6a183295a81e6e7625
Freek Dijkstra <freek@macfreek.nl>
2018-05-06 20:35:48 +0200

    Code to generate MERIL output

--- generate_map.py
+++ generate_map.py
@@ -124,9 +124,25 @@ def get_meril_nodes():
                 organsationAddresses.append(organisations[organisation]["Postal Address"])
             except KeyError:
                 pass
-        # TODO: do something useful here
-        # print(identifier, locationtypes, locationarray, organsationAddresses)
-        # places[...] = ...
+        place = {
+            'meril_id': identifier,
+            'name': infrastructure['name'],
+            'url': infrastructure.get('url'),
+            'address': address,
+            'town': town,
+            'zipcode': zipcode,
+            'region': region,
+            'country': location_country,
+            'provider_country': provider_country,
+            'typeOfRI': locationtypes,  # list
+            'keywords': infrastructure.get("Scientific Description", {}).get("riKeywords", []),
+            'categories': infrastructure.get("Classifications", {}).get("riCategory", []),
+            'esfriDomain': infrastructure.get("scientificDomain", {}).get("esfriDomain", []),
+        }
+        places[identifier] = place
+        # TODO: Debug
+        # if len(places) > 20:
+        #     break
     return places
 
 
@@ -256,19 +272,19 @@ def umap_instruments_layer(esfri_data):
 def umap_meril_layer(meril_data):
     """Return a geojson.FeatureCollection"""
     geolayer = geojson.FeatureCollection([])
-    for city in meril_data:
+    for city in meril_data.values():
         geom = geojson.Point((city['long'], city['lat']))
         props = {
-                    "name": city['full_name'],
+                    "name": city['name'],
                     # for geojson.io:
                     # "marker-color": "#7b7e42",
                     # "marker-size": "small",
                     # "marker-symbol": "telephone",
                     # for umap.openstreetmap.fr:
                     "_storage_options": {
-                      "iconUrl": "https://raw.githubusercontent.com" \
-                                 "/sara-nl/data-cartography/master/symbols/microscope.png",
-                      "iconClass": "Drop",
+                      # "iconUrl": "https://raw.githubusercontent.com" \
+                      #            "/sara-nl/data-cartography/master/symbols/microscope.png",
+                      "iconClass": "Circle",
                       "color": "#EE3322",
                       "popupTemplate": "Default",
                       "showLabel": "true",
@@ -387,7 +403,7 @@ def parse_and_filter_sc(sc_data, country_filter):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, stream=sys.stderr, 
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stderr, 
                         format='%(levelname)-8s %(message)s')
     config = ConfigParser()
     config.read('config.ini')
@@ -396,27 +412,29 @@ if __name__ == '__main__':
     locator = Locator(downloader=downloader)
     country_filter = locator.filter_factory(locator.eu_countries)
 
-    geant_data = get_geant_nodes(downloader)
-    geant_nodes, geant_links = parse_and_filter_network(geant_data, country_filter)
-    logging.info("%d node and %d links in GEANT network" % (len(geant_nodes), len(geant_links)))
-    geolayer = umap_network_layer(geant_nodes, geant_links)
-    export_geojson(OUTPUT_FILENAME_NETWORK, geolayer)
-
-    sc_data = get_top500_nodes(downloader)
-    sc_nodes = parse_and_filter_sc(sc_data, country_filter)
-    locator.locate_and_filter_places(sc_nodes)
-    logging.info("%d supercomputer centers" % (len(sc_nodes)))
-    geolayer = umap_sc_layer(sc_nodes.values())
-    export_geojson(OUTPUT_FILENAME_SUPERCOMPUTERS, geolayer)
-
-    esfri_nodes = get_esfri_nodes()
-    locator.locate_and_filter_places(esfri_nodes)  # no filtering
-    logging.info("%d ESFRI-listed single-site instruments" % (len(esfri_nodes)))
-    geolayer = umap_instruments_layer(esfri_nodes)
-    export_geojson(OUTPUT_FILENAME_INSTRUMENTS, geolayer)
+    # geant_data = get_geant_nodes(downloader)
+    # geant_nodes, geant_links = parse_and_filter_network(geant_data, country_filter)
+    # logging.info("%d node and %d links in GEANT network" % (len(geant_nodes), len(geant_links)))
+    # geolayer = umap_network_layer(geant_nodes, geant_links)
+    # export_geojson(OUTPUT_FILENAME_NETWORK, geolayer)
+    #
+    # sc_data = get_top500_nodes(downloader)
+    # sc_nodes = parse_and_filter_sc(sc_data, country_filter)
+    # locator.locate_and_filter_places(sc_nodes)
+    # logging.info("%d supercomputer centers" % (len(sc_nodes)))
+    # geolayer = umap_sc_layer(sc_nodes.values())
+    # export_geojson(OUTPUT_FILENAME_SUPERCOMPUTERS, geolayer)
+    #
+    # esfri_nodes = get_esfri_nodes()
+    # locator.locate_and_filter_places(esfri_nodes)  # no filtering
+    # logging.info("%d ESFRI-listed single-site instruments" % (len(esfri_nodes)))
+    # geolayer = umap_instruments_layer(esfri_nodes)
+    # export_geojson(OUTPUT_FILENAME_INSTRUMENTS, geolayer)
 
     meril_nodes = get_meril_nodes()
     locator.locate_and_filter_places(meril_nodes)  # no filtering
     logging.info("%d MERIL-listed instruments" % (len(meril_nodes)))
     geolayer = umap_meril_layer(meril_nodes)
     export_geojson(OUTPUT_FILENAME_MERIL, geolayer)
+    
+    locator.store_known_places()
